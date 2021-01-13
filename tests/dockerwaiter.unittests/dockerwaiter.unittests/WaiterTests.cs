305cc5f45f90c9e21de22747ae58c8ee7d95f08c
José Manuel Corral <jcorral@plainconcepts.com>
2021-01-13 17:37:40 +0100

    Changed to pool

--- tests/dockerwaiter.unittests/dockerwaiter.unittests/WaiterTests.cs
+++ tests/dockerwaiter.unittests/dockerwaiter.unittests/WaiterTests.cs
@@ -7,15 +7,20 @@ namespace dockerwaiter.unittests
     public class WaiterTests
     {
         [Fact]
-        public void TestMonitorContainers()
+        public async Task TestMonitorContainers()
         {
             var arguments = new WaiterArguments() { ContainersToFilter = ["container1", "container2"], DockerCompose = "docker-compose.yml", LogPath = "", Timeout = 3600 };
 
-            var waiter = new Waiter(arguments, FakeContainerHelper.Setup(x=> {
-                this.
-            }));
-            Task. waiter.Execute();
+            var containerHelper = FakeContainerHelper.Setup(//Pasarle la coleccion de contenedores, 
+                                                            //accion a ejecutar dentro del fakeContainer 
+                                                            // tal vez un exitContainer("nombre", 0, millisegundos cuando pasará )
+                                                            );
+                )
+            var waiter = new Waiter(arguments, containerHelper);
+            var result = await waiter.Execute();
+            Assert.True(result == 0);
 
+            
         }
     }
 }
